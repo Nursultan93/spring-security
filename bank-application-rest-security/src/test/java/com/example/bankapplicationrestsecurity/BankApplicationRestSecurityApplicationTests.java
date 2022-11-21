476513f8e0546e49b2f0c6f7ad0ba313d158eef7
Nursultan <Nursultan.Xalilov@kapitalbank.az>
2022-11-21 11:01:37 +0400

    add gradle

---
+++ bank-application-rest-security/src/test/java/com/example/bankapplicationrestsecurity/BankApplicationRestSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bankapplicationrestsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApplicationRestSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
